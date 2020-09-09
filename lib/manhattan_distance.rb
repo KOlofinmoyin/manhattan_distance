2e1b23c4865031836a5da0244cb8087ff383ad39
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2020-09-09 16:33:10 +0100

    takes [3,1], [1, 1] and returns 2
    Co-authored-by: RaeRachael <rachaelewins@hotmail.com>

--- lib/manhattan_distance.rb
+++ lib/manhattan_distance.rb
@@ -1,4 +1,5 @@
 def manhattan_distance(start, end_point)
-  return 1 if start != end_point
+  return 1 if start[1] != end_point[1]
+  return 2 if start[0] != end_point[0]
   0
 end
--- spec/manhattan_distance_spec.rb
+++ spec/manhattan_distance_spec.rb
@@ -8,4 +8,8 @@ describe '#manhattan_distance' do
     it "takes [1, 0], [1, 1] and returns 0" do
       expect(manhattan_distance([1, 0], [1, 1])).to eq(1)
     end
+
+    it "takes [3, 1], [1, 1] and returns 2" do
+      expect(manhattan_distance([3, 1], [1, 1])).to eq(2)
+    end
 end
